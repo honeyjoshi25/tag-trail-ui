aa4353535e44bc15396ec61e5cc4e210b527b142
hjoshi13 <honey.joshi@inferenz.ai>
2024-05-21 17:05:24 +0530

    [Update]deprecated modules

--- src/Redux/Reducer/Auth_Reducer.js
+++ src/Redux/Reducer/Auth_Reducer.js
@@ -1,16 +1,15 @@
-import { AUTH, LOGOUT } from '../Constants/ActionTypes'
+import { AUTH, LOGOUT } from "../Constants/ActionTypes";
 
 export const authReducer = (state = { authData: null }, action) => {
   switch (action.type) {
     case AUTH:
-      console.log(action?.data)
-      localStorage.setItem('profile', JSON.stringify({ ...action?.data }))
-      return { ...state, authData: action?.data }
+      localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
+      return { ...state, authData: action?.data };
 
     case LOGOUT:
-      localStorage.clear()
-      return { ...state, authData: null }
+      localStorage.clear();
+      return { ...state, authData: null };
     default:
-      return state
+      return state;
   }
-}
+};
